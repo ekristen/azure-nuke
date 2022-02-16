960e4d1b4ac944b543c785f2019dc896655b29a3
Erik Kristensen <erik@erikkristensen.com>
2022-02-16 00:04:06 +0000

    add(application-secret): adding azure ad application secrets

---
+++ resources/application-secret.go
@@ -0,0 +1,90 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/manicminer/hamilton/auth"
+	"github.com/manicminer/hamilton/msgraph"
+	"github.com/manicminer/hamilton/odata"
+	"github.com/sirupsen/logrus"
+
+	"github.com/ekristen/azure-nuke/pkg/resource"
+	"github.com/ekristen/azure-nuke/pkg/types"
+)
+
+func init() {
+	resource.RegisterV2(resource.Registration{
+		Name:   "ApplicationSecret",
+		Scope:  resource.Tenant,
+		Lister: ListApplicationSecret,
+	})
+}
+
+type ApplicationSecret struct {
+	client *msgraph.ApplicationsClient
+	id     *string
+	name   *string
+	appId  *string
+}
+
+func (r *ApplicationSecret) Filter() error {
+	return nil
+}
+
+func (r *ApplicationSecret) Remove() error {
+	_, err := r.client.Delete(context.TODO(), *r.id)
+	return err
+}
+
+func (r *ApplicationSecret) Properties() types.Properties {
+	properties := types.NewProperties()
+
+	properties.Set("Name", *r.name)
+
+	return properties
+}
+
+func (r *ApplicationSecret) String() string {
+	return *r.id
+}
+
+func ListApplicationSecret(opts resource.ListerOpts) ([]resource.Resource, error) {
+	logrus.Tracef("subscription id: %s", opts.SubscriptionId)
+
+	wrappedAuth, err := auth.NewAutorestAuthorizerWrapper(opts.Authorizers.Graph)
+	if err != nil {
+		return nil, err
+	}
+
+	client := msgraph.NewApplicationsClient(opts.TenantId)
+	client.BaseClient.Authorizer = wrappedAuth
+	client.BaseClient.DisableRetries = true
+
+	resources := make([]resource.Resource, 0)
+
+	logrus.Trace("attempting to list service principals")
+
+	ctx := context.Background()
+
+	entites, _, err := client.List(ctx, odata.Query{})
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.Trace("listing ....")
+
+	for _, entity := range *entites {
+		for _, cred := range *entity.PasswordCredentials {
+			fmt.Println("secret")
+			resources = append(resources, &ApplicationSecret{
+				client: client,
+				id:     cred.KeyId,
+				name:   cred.DisplayName,
+				appId:  entity.ID,
+			})
+		}
+	}
+
+	return resources, nil
+}
