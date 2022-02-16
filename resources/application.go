ba8e196cb5d1692d1e84530c8f65382851a091b5
Erik Kristensen <erik@erikkristensen.com>
2022-02-16 00:03:50 +0000

    add(application): adding azure ad application

---
+++ resources/application.go
@@ -0,0 +1,84 @@
+package resources
+
+import (
+	"context"
+
+	"github.com/manicminer/hamilton/auth"
+	"github.com/manicminer/hamilton/msgraph"
+	"github.com/manicminer/hamilton/odata"
+	"github.com/sirupsen/logrus"
+
+	"github.com/ekristen/azure-nuke/pkg/resource"
+	"github.com/ekristen/azure-nuke/pkg/types"
+)
+
+type Application struct {
+	client *msgraph.ApplicationsClient
+	id     *string
+	name   *string
+}
+
+func init() {
+	resource.RegisterV2(resource.Registration{
+		Name:   "Application",
+		Scope:  resource.Tenant,
+		Lister: ListApplication,
+	})
+}
+
+func ListApplication(opts resource.ListerOpts) ([]resource.Resource, error) {
+	logrus.Tracef("subscription id: %s", opts.SubscriptionId)
+
+	wrappedAuth, err := auth.NewAutorestAuthorizerWrapper(opts.Authorizers.Graph)
+	if err != nil {
+		return nil, err
+	}
+
+	client := msgraph.NewApplicationsClient(opts.TenantId)
+	client.BaseClient.Authorizer = wrappedAuth
+	client.BaseClient.DisableRetries = true
+
+	resources := make([]resource.Resource, 0)
+
+	logrus.Trace("attempting to list service principals")
+
+	ctx := context.Background()
+
+	entites, _, err := client.List(ctx, odata.Query{})
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.Trace("listing ....")
+
+	for _, entity := range *entites {
+		resources = append(resources, &Application{
+			client: client,
+			id:     entity.ID,
+			name:   entity.DisplayName,
+		})
+	}
+
+	return resources, nil
+}
+
+func (r *Application) Filter() error {
+	return nil
+}
+
+func (r *Application) Remove() error {
+	_, err := r.client.Delete(context.TODO(), *r.id)
+	return err
+}
+
+func (r *Application) Properties() types.Properties {
+	properties := types.NewProperties()
+
+	properties.Set("Name", *r.name)
+
+	return properties
+}
+
+func (r *Application) String() string {
+	return *r.id
+}
