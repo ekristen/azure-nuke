eec8284fb4b06757d22bfd9eec35a392a26081ad
Erik Kristensen <erik@erikkristensen.com>
2022-06-01 14:59:24 +0000

    chore: remove unused code

--- pkg/azure/types.go
+++ pkg/azure/types.go
@@ -1,29 +1,9 @@
 package azure
 
 import (
-	"time"
-
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/manicminer/hamilton/auth"
 )
-
-// authResult contains the subset of results from token acquisition operation in ConfidentialClientApplication
-// For details see https://aka.ms/msal-net-authenticationresult
-type authResult struct {
-	accessToken    string
-	expiresOn      time.Time
-	grantedScopes  []string
-	declinedScopes []string
-}
-
-// OAuthToken implements the OAuthTokenProvider interface.  It returns the current access token.
-func (ar authResult) OAuthToken() string {
-	return ar.accessToken
-}
-
-func (ar authResult) Token() string {
-	return ar.accessToken
-}
 
 type Authorizers struct {
 	Graph      auth.Authorizer
