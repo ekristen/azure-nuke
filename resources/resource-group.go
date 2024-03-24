f38ea89a5b474dbb5edae2df8d07d385728481a0
Erik Kristensen <erik@erikkristensen.com>
2024-03-24 12:28:29 -0600

    fix(resource-group): fix subscriptionId from being nil

--- resources/resource-group.go
+++ resources/resource-group.go
@@ -31,7 +31,7 @@ type ResourceGroup struct {
 	client         *resourcegroups.ResourceGroupsClient
 	name           *string
 	location       string
-	subscriptionId *string
+	subscriptionId string
 }
 
 func (r *ResourceGroup) Remove(ctx context.Context) error {
@@ -44,6 +44,7 @@ func (r *ResourceGroup) Properties() types.Properties {
 
 	properties.Set("Name", r.name)
 	properties.Set("Location", r.location)
+	properties.Set("SubscriptionId", r.subscriptionId)
 
 	return properties
 }
@@ -84,9 +85,10 @@ func (l ResourceGroupLister) List(ctx context.Context, o interface{}) ([]resourc
 
 	for _, entity := range *list.Model {
 		resources = append(resources, &ResourceGroup{
-			client:   client,
-			name:     entity.Name,
-			location: entity.Location,
+			client:         client,
+			name:           entity.Name,
+			location:       entity.Location,
+			subscriptionId: opts.SubscriptionId,
 		})
 	}
 
